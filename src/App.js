82e209d043eaac21cafad52c17ad3ab633beac3f
MBembers <macbed1234@gmail.com>
2022-12-20 18:22:43 +0100

    board

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Tile from "./Tile";
+import React, { useState } from "react";
+import * as emptyBoard from "./data/helpers.json";
+import Game from "./Game";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [board, setBoard] = useState(new Array(81).fill(new Tile()));
+  const game = new Game(board, setBoard);
+  console.log(board);
+  let renderBoard = () => {
+    return board.map((tile, i) => {
+      return (
+        <div
+          className={`tile correct-${tile.correct} 
+          solved-${tile.solved} 
+          selected-${tile.selected}`}
+          key={i}
         >
-          Learn React
-        </a>
-      </header>
+          {tile.number}
+        </div>
+      );
+    });
+  };
+  return (
+    <div className="main">
+      <div className="panel panel-left"></div>
+      <div className="center">
+        <div className="board">{renderBoard()}</div>
+      </div>
+      <div className="panel panel-right"></div>
     </div>
   );
 }
 
 export default App;
+
+/*
+Instrukcje
+Projekt sudoku (svelte/react)
+
+5pkt
+    plansza-szachownica 9x9
+    wartości wpisywane z klawiatury, akcentowanie kolorem dobrych i złych odpowiedzi
+    możliwość przemieszczania się strzałkami, kasowania wcześniejszej wartości
+    możliwość wydrukowania rozwiązanego zestawu sudoku - planszy z tym co wpisane - @media print
+    responsywność
+
+6pkt
+    wczytanie zestawu sudoku z zewnętrznego pliku, tych wartości nie można edytować
+    możliwość samodzielnego rozwiązania wczytanego zestawu *
+    możliwość automatycznego rozwiązania wczytanego zestawu *
+    możliwość ustawiania "hint" (urzytkownik (osoba korzystajaca ze strony) ustala co może znajdować się w danym polu), "erease"
+    wyświetlanie informacji o tym jakie cyfry zostały jeszcze do wpisania
+    możliwość zapisania aktualnego stanu gry do pliku
+
+* rozpoznawanie sytuacji gdy zestaw jest nie rozwiązywalny
+** inne technologie niż svelte uzgadniamy indywidualnie
+
+W załączniku znajduje się przykładowy design aplikacji
+*/
---
+++ src/Tile.js
@@ -0,0 +1,8 @@
+export default class Tile {
+  selected = false;
+  hints = [];
+  style = null;
+  correct = true;
+  number = 0;
+  solved = false;
+}
